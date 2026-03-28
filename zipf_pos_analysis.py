a8647278c545eeedfbc8d28b92db7cb4402dafb6
janders-code <kalebjanders@gmail.com>
2026-03-27 22:38:46 -0700

    Implement CSV export for word data
    Added functionality to export word data to a CSV file.

--- zipf_pos_analysis.py
+++ zipf_pos_analysis.py
@@ -3,13 +3,15 @@
 # kga56@sfu.ca
 # Project: zipfs-law-pos-brown-corpus
 import nltk
+import csv
+
 nltk.download('brown')
 from nltk.corpus import brown
 from nltk import FreqDist
-# load tagged words with universal tagset
+
 tagged_words = brown.tagged_words(tagset='universal')
-# map the categories to the universal tagset
-function_tags = {'DET', 'ADP', 'PRON', 'CONJ', 'ADV', 'PRT'}
+
+function_tags = {'DET', 'ADP', 'PRON', 'CONJ', 'PRT'}
 
 category_map = {
     'NOUN': 'noun',
@@ -19,43 +21,39 @@ category_map = {
 for tag in function_tags:
     category_map[tag] = 'function'
 
-# put brown words into word_categories
 words_categorized = [] 
-
 for word, tag in tagged_words:
     category = category_map.get(tag)
     if category:
         words_categorized.append((word, category))
 
-#print(words_categorized[:20])
-#print(len(words_categorized))
-
-#clean the words by making them lowercase and remove not words
 words_cleaned = []
-
 for word, category in words_categorized:
     word = word.lower()
     if word.isalpha():
         words_cleaned.append((word, category))
-print(len(words_categorized))
-print(len(words_cleaned))
 
-# checking frequency 
 freq = FreqDist(word for word, category in words_cleaned)
-print(freq.most_common(20))
-
-
-# dictionary that stores each unique word with its category, frequency and word legnth together
 
 word_data = {}
-
 for word, category in words_cleaned:
-    if freq[word] >= 5:  #  min frequency filter
+    if freq[word] >= 5:
         if word not in word_data:
             word_data[word] = {
                 'category': category,
                 'frequency': freq[word],
                 'length': len(word)
             }
-for word in list(word_data.keys())[:5]:
-    print(word, word_data[word])
+
+with open('word_data.csv', 'w', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=['word', 'category', 'frequency', 'length'])
+    writer.writeheader()
+    for word, data in word_data.items():
+        writer.writerow({
+            'word': word,
+            'category': data['category'],
+            'frequency': data['frequency'],
+            'length': data['length']
+        })
+
+
